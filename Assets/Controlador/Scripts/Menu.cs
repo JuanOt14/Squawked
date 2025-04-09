60e14735218adeaf9195e6715249ac0b094917f8
juanegi <131917495+juanegi@users.noreply.github.com>
2025-04-09 18:04:33 -0500

    actualizacion de las mecanicas del juego

--- Assets/Controlador/Scripts/Menu.cs
+++ Assets/Controlador/Scripts/Menu.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using  UnityEngine.SceneManagement;
-
+//este codigo es para cambiar de escena dentro del juego pasa del meniu al tutorial
+// y de la escena del tutorial a la escena de juego 
 public class Menu : MonoBehaviour
 {
-    public void StartMision04(string NombreNivel){
+    public void Startutorial(string NombreNivel){
         SceneManager.LoadScene(NombreNivel);
     }
     public void Salir(){
         Application.Quit();
     }
 }
+//para cambiar de escenas con los botones de unity el script arrastrarlo encima del boton
+//y usar preferiblemente la funcion on click
---
+++ Assets/Controlador/Scripts/Puerta.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Puerta : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pato")
+        {
+            Debug.Log("Trae el objeto");
+        }
+        if (other.tag == "Objeto")
+        {
+            Debug.Log("Si esta haciendo lo que necesito");
+
+            Destroy(gameObject);
+            SceneManager.LoadScene("Mision 123");
+        }
+
+    }
+}
